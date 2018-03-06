132962ed0397c58680dd03bd9184b221eb398539
zhongfly <icesshadows@gmail.com>
2018-03-06 19:42:55 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,194 @@
+# encoding:UTF-8
+# python3.6
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Job, CallbackQueryHandler, ConversationHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from postman import islogin, pre_login, login, post_news, dailybonus, get_news, load_cookies
+import logging
+import re
+import time
+import os
+import requests
+from functools import wraps
+
+token = 'xxxxxx'
+admin = [12345678]
+updater = Updater(token, request_kwargs={
+    'proxy_url': 'socks5://127.0.0.1:1080/'
+})
+s = requests.Session()
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+if os.path.exists('cookie.txt'):
+    load_cookies(session=s)
+else:
+    pass
+
+GETURL, CONFIRM, GETANSWER, news = range(4)
+
+
+def restricted(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in admin:
+            update.message.reply_text('你没有操作BOT的权限', quote=True)
+            return
+        return func(bot, update, *args, **kwargs)
+    return wrapped
+
+
+@restricted
+def start_handler(bot, update):
+    chat_id = update.message.chat_id
+    bot.sendMessage(text="/islogin 检查是否登录\n/login 登录\n/dailybonus 每日打卡\n/post 发帖",
+                    chat_id=chat_id)
+
+
+@restricted
+def islogin_handler(bot, update):
+    chat_id = update.message.chat_id
+    result = islogin(s)
+    logger.info('检查登录状态，结果为“{}”'.format(result))
+    update.message.reply_text(result, quote=True)
+
+
+@restricted
+def dailybonus_handler(bot, update):
+    chat_id = update.message.chat_id
+    result = dailybonus(s)
+    logger.info('每日打卡，结果为“{}”'.format(result))
+    update.message.reply_text(result, quote=True)
+
+
+def post_timeout(bot, job):
+    bot.editMessageText(
+        chat_id=job.context[0], message_id=job.context[1], text='已结束等待')
+    return post_handler.END
+
+
+@restricted
+def post_start(bot, update, job_queue):
+    chat_id = update.message.chat_id
+    logger.info('开始发帖流程')
+    msg = update.message.reply_text('等待输入网址。。。\n/cancel 取消', quote=True)
+    global j
+    j = job_queue.run_once(post_timeout, 30, context=(chat_id, msg.message_id))
+    return GETURL
+
+
+@restricted
+def geturl(bot, update):
+    j.schedule_removal()
+    text = update.message.text
+    chat_id = update.message.chat_id
+    logger.info("收到链接{}，进行分析".format(text))
+    global news
+    news = get_news(text)
+    if news != 0:
+        confirm_btn = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text='确认', callback_data='ok'),
+             InlineKeyboardButton(text='取消', callback_data='cancel')]
+        ])
+        msg = update.message.reply_text(
+            news['title'], quote=True, reply_markup=confirm_btn)
+        logger.info("链接分析结果：“{}”".format(news['title']))
+        return CONFIRM
+    else:
+        logger.info('链接无法识别')
+        update.message.reply_text('链接无法识别,请重试', quote=True)
+        return GETURL
+
+
+@restricted
+def confirm_callback(bot, update):
+    query = update.callback_query
+    if query.data == 'ok':
+        result = post_news(news,s)
+        logger.info("执行发帖操作，标题为“{}”，结果为：【{}】".format(news['title'], result))
+        bot.sendMessage(text=result, chat_id=query.message.chat_id)
+    else:
+        logger.info("确认未通过，取消发帖操作。")
+        bot.sendMessage(text='已取消操作', chat_id=query.message.chat_id)
+        my_news = None
+    logger.info("结束会话。")
+    return ConversationHandler.END
+
+
+def cancel(bot, update):
+    user = update.message.from_user
+    news = None
+    logger.info("取消操作，结束会话。", user.first_name)
+    update.message.reply_text('已取消', quote=True)
+    return ConversationHandler.END
+
+
+post_handler = ConversationHandler(
+    entry_points=[CommandHandler('post', post_start, pass_job_queue=True)],
+
+    states={
+        GETURL: [MessageHandler(Filters.text, geturl)],
+        CONFIRM: [CallbackQueryHandler(confirm_callback)],
+    },
+
+    fallbacks=[CommandHandler('cancel', cancel)],
+
+    allow_reentry=True,
+    conversation_timeout=30,
+)
+
+
+def login_start(bot, update):
+    chat_id = update.message.chat_id
+    if islogin(s) == '已登录':
+        update.message.reply_text('已经登录，请勿重复登录', quote=True)
+        return ConversationHandler.END
+    else:
+        pass
+    pre_login(s, openimg=False)
+    bot.send_photo(chat_id=chat_id, photo=open('capt.png', 'rb'),
+                   reply_to_message_id=update.message.message_id)
+    return GETANSWER
+
+
+def getanswer(bot, update):
+    answer = update.message.text
+    msg = update.message.reply_text('正在登录ing', quote=True)
+    result = login(answer,s)
+    bot.editMessageText(
+        chat_id=update.message.chat_id, message_id=msg.message_id, text=result)
+    return ConversationHandler.END
+
+
+login_handler = ConversationHandler(
+    entry_points=[CommandHandler('login', login_start)],
+
+    states={
+        GETANSWER: [MessageHandler(Filters.text, getanswer)],
+    },
+
+    fallbacks=[CommandHandler('cancel', cancel)],
+
+    allow_reentry=True,
+
+    conversation_timeout=30,
+)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+updater.dispatcher.add_handler(CommandHandler('start', start_handler))
+updater.dispatcher.add_handler(CommandHandler('islogin', islogin_handler))
+updater.dispatcher.add_handler(
+    CommandHandler('dailybonus', dailybonus_handler))
+updater.dispatcher.add_handler(post_handler)
+updater.dispatcher.add_handler(login_handler)
+updater.dispatcher.add_error_handler(error)
+
+updater.start_polling()
+updater.idle()
---
+++ postman.py
@@ -0,0 +1,268 @@
+# encoding:UTF-8
+# python3.6
+import os
+import re
+import json
+import time
+import datetime
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+from PIL import Image
+from requests_toolbelt import MultipartEncoder
+
+pwuser = 'xxxx'#用户名
+pwpwd = 'xxxx'#密码
+question = '1'#验证问题序号（从上到下）
+answer = 'xxxx'#验证问题答案
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) \
+                    AppleWebKit/537.36 (KHTML, like Gecko) \
+                    Chrome/60.0.3112.90 Safari/537.36',
+}
+proxies = {
+    'http': 'socks5://127.0.0.1:1080',
+    'https': 'socks5://127.0.0.1:1080'
+}
+defalt_proxies = proxies#如果能直接访问，则填None
+
+
+def save_cookies(session, file='cookie.txt'):
+    with open(file, 'w') as f:
+        json.dump(requests.utils.dict_from_cookiejar(session.cookies), f)
+
+
+def load_cookies(session, file='cookie.txt'):
+    with open(file, 'r') as f:
+        session.cookies = requests.utils.cookiejar_from_dict(json.load(f))
+
+
+def islogin(session):
+    if os.path.exists('cookie.txt'):
+        load_cookies(session=session)
+    else:
+        pass
+    try:
+        r = session.get('http://moeshare.com', headers=headers,
+                        proxies=defalt_proxies)
+    except Exception as e:
+        r = session.get('http://moeshare.com',
+                        headers=headers, proxies=proxies)
+    soup = BeautifulSoup(r.text, 'lxml')
+    flag = soup.find(class_='login fr')
+    if flag != None:
+        result = '已登录'
+    else:
+        result = '未登录'
+    return result
+
+
+def pre_login(session, openimg=True):
+    try:
+        r = session.get('http://moeshare.com/login.php#breadCrumb',
+                        headers=headers, proxies=defalt_proxies)
+    except Exception as e:
+        r = session.get('http://moeshare.com/login.php#breadCrumb',
+                        headers=headers, proxies=proxies)
+    soup = BeautifulSoup(r.text, 'lxml')
+    divs = soup.find_all('img')
+    try:
+        capt = session.get(urljoin('http://moeshare.com',
+                                   divs[-1]['src']), headers=headers, stream=True, proxies=defalt_proxies)
+    except Exception as e:
+        capt = session.get(urljoin('http://moeshare.com',
+                                   divs[-1]['src']), headers=headers, stream=True, proxies=proxies)
+    with open('capt.png', 'wb') as f:
+        for chunk in capt.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+    if openimg == True:
+        with Image.open("capt.png") as im:
+            im.show()
+    else:
+        pass
+
+
+def login(qanswer, session):
+    try:
+        os.remove('capt.png')
+    except Exception as e:
+        raise
+    data = {
+        'answer': answer
+        'cktime': '31536000',
+        'hideid': '0',
+        'jumpurl': 'index.php',
+        'lgt': '0',
+        'pwpwd': pwpwd,
+        'pwuser': pwuser,
+        'qanswer': qanswer,
+        'qkey': '-1',
+        'question': question,
+        'step': '2',
+    }
+    try:
+        r = session.post('http://moeshare.com/login.php?',
+                         data=data, headers=headers, proxies=defalt_proxies)
+    except Exception as e:
+        r = session.post('http://moeshare.com/login.php?',
+                         data=data, headers=headers, proxies=proxies)
+    r.encoding = 'utf-8'
+    soup = BeautifulSoup(r.text, 'lxml')
+    flag = soup.find('span')
+    if flag != None and flag.text == '您已经顺利登录':
+        result = '登录成功'
+        save_cookies(session)
+    else:
+        result = '登录失败'
+    return result
+
+
+def post_news(news, session):
+    try:
+        r = session.get('http://moeshare.com/post.php?fid=17',
+                        headers=headers, proxies=defalt_proxies)
+    except Exception as e:
+        r = session.get('http://moeshare.com/post.php?fid=17',
+                        headers=headers, proxies=proxies)
+    soup = BeautifulSoup(r.text, 'lxml')
+    verify = soup.find('input', attrs={'name': 'verify'})['value']
+    hexie = soup.find(
+        'input', attrs={'name': '_hexie'}).find_next_sibling('script').text[-11:-3]
+    fields = {
+        "magicname": '',
+        "magicid": '',
+        "verify": verify,
+        "cyid": '0',
+        "ajax": '1',
+        "iscontinue": '0',
+        "atc_usesign": '1',
+        "atc_autourl": '1',
+        "atc_convert": '1',
+        "atc_money": '1',
+        "atc_credittype": 'money',
+        "atc_rvrc": '0',
+        "atc_enhidetype": '0',
+        "atc_title": news['title'],  # 标题
+        "atc_iconid": '0',
+        "p_type": '59',
+        "p_sub_type": '0',
+        "atc_content": news['content'],  # 正文
+        "att_special1": '0',
+        "att_ctype1": 'money',
+        "atc_needrvrc1": '0',
+        "step": '2',
+        "action": 'new',
+        "fid": '17',
+        "tid": '0',
+        "article": '0',
+        "special": '0',
+        "_hexie": hexie,
+    }
+    m = MultipartEncoder(fields, boundary='WebKitFormBoundary0ABbhG1IDBxTLQam')
+    nowtime = str(round(time.time() * 1000))  # 毫秒时间戳
+    post_url = 'http://moeshare.com/post.php?fid=17&nowtime='+nowtime+'&verify='+verify
+    new = headers
+    new['Content-Type'] = m.content_type
+    try:
+        r = session.post(post_url, data=m, headers=new, proxies=defalt_proxies)
+    except Exception as e:
+        r = session.post(post_url, data=m, headers=new, proxies=proxies)
+    pattern = re.compile(r'(?<=(\[CDATA\[)).+(?=(\]\]></ajax>))')
+    #'<?xml version="1.0" encoding="utf-8"?><ajax><![CDATA[success\tread.php?tid=145492]]></ajax>'
+    result = pattern.search(r.text.replace('\t', ' '))
+    if result != None:
+        text = result.group().split(' ')
+        if text[0] == 'success':
+            text[1] = urljoin('http://moeshare.com', text[1])
+            result = '{}\n{}'.format(text[0], text[1])
+        else:
+            result = result.group()
+    else:
+        result = '未返回结果'
+    save_cookies(session)
+    return result
+
+
+def dailybonus(session):
+    try:
+        r = session.get('http://moeshare.com/u.php',
+                        headers=headers, proxies=defalt_proxies)
+    except Exception as e:
+        r = session.get('http://moeshare.com/u.php',
+                        headers=headers, proxies=proxies)
+    r.encoding = 'utf-8'
+    soup = BeautifulSoup(r.text, 'lxml')
+    verify = soup.find('input', attrs={'name': 'verify'})['value']
+    nowtime = str(round(time.time() * 1000))
+    post_url = 'http://moeshare.com/jobcenter.php?action=punch&verify=' + \
+        verify+'&nowtime='+nowtime+'&verify='+verify
+    try:
+        r = session.post(
+            post_url, data={'step': '2', }, headers=headers, proxies=defalt_proxies)
+    except Exception as e:
+        r = session.post(
+            post_url, data={'step': '2', }, headers=headers, proxies=proxies)
+    r.encoding = 'utf-8'  # 纠正编码
+    pattern = re.compile('(?<=(\"message\":\')).+(?=(\',\"flag\"))')
+    result = pattern.search(r.text).group()
+    save_cookies(session)
+    return result
+
+
+def get_news(url):
+    news = {}
+    acgdog_pattern = re.compile(r'http(s)?://www\.acgdoge\.net/archives/\d+')
+    dmzj_pattern = re.compile(
+        r'http(s)?://(m)?news\.dmzj\.com/article/\d+\.html')
+    if acgdog_pattern.search(url):
+        url = acgdog_pattern.search(url).group()
+        r = requests.get(url, headers=headers)
+        r.encoding = 'utf-8'  # 纠正编码
+        # soup=BeautifulSoup(r.text, "lxml")
+        # soup=soup.find('hgroup',attrs={'class':"post_hctn"}).parent
+        # lxml真是有毒，就是不能直接找到我要的tag,html5lib就可以
+        soup = BeautifulSoup(r.text, "html5lib")
+        soup = soup.find('article')
+        title = soup.find(class_="post_h").a.text
+        post_time = soup.find(class_="post_t_d").text.split('/')
+        mon = int(post_time[0])
+        day = int(post_time[1])
+        china_time = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
+        china_time = china_time.timetuple()
+        if mon > china_time[1]:
+            year = china_time[0]-2001
+        else:
+            year = china_time[0]-2000
+        news['title'] = '[{0}{1:02}{2:02}]{3}'.format(
+            str(year), mon, day, title)
+        content_divs = soup.find(class_="post_t").find_all('p')
+        img_src = 'data-lazy-src'
+    elif dmzj_pattern.search(url):
+        url = dmzj_pattern.search(url).group().replace('mnews', 'news')
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, "lxml")
+        soup = soup.find(class_="news_content autoHeight")
+        title = soup.find('h1').text
+        time = soup.find(class_="data_time").text[2:10].replace('-', '')
+        news['title'] = '[{0}]{1}'.format(time, title)
+        content_divs = soup.find(class_="news_content_con").find_all('p')
+        img_src = 'src'
+    else:
+        return 0
+    content = '[url]{}[/url]'.format(url)+'\n'
+    for div in content_divs:
+        if div.text == '':
+            if div.find('img') != None:
+                img_url = div.find('img')[img_src]
+                content = content+'[img]{}[/img]\n'.format(img_url)
+            else:
+                pass
+        else:
+            content = content+'{}\n'.format(div.text)
+    news['content'] = content
+    return news
+
+
